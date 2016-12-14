70d9003d632a43baabfa61f22c19a81f00698484
Administrator <Administrator@abo.com>
2016-12-14 11:29:17 +0800

    我

--- src/main/java/com/jxlt/stage/wechat/web/JX_ProjectController.java
+++ src/main/java/com/jxlt/stage/wechat/web/JX_ProjectController.java
@@ -84,9 +84,12 @@ public class JX_ProjectController {
 		List<Project> listPro = new ArrayList<Project>();
 		try {
 			String contractNum = req.getParameter("contractNum");
+			System.out.println(contractNum);
 			contractNum = new String(contractNum.getBytes("iso-8859-1"),"utf-8");
-			User user = (User)req.getSession().getAttribute("loginUser"); 
-			user = jxProjectService.getUserById(user.getId());
+			System.out.println(contractNum);
+			String userId = req.getParameter("userId");
+			System.out.println(userId);
+			User user = jxProjectService.getUserById(Integer.valueOf(userId));
 			if(user != null){
 				listPro = jxProjectService.getProjectListByConNum(contractNum,user.getUtype(),user.getId());
 			}
